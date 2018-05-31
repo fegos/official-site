7da7e98b3955b207e21a557ed98aba56b7b6d24f
esky <esky3@163.com>
2018-05-31 14:59:18 +0800

    备案号

--- client/desktop/frame/Footer.js
+++ client/desktop/frame/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return (
       <div id="footer" >
         <div className="g-ctn f-cb">
-          <p>Copyright © FEGO大前端团队</p>
+          <p>Copyright © FEGO大前端团队 <em>蜀ICP备17022216号-2</em></p>
         </div>
       </div>
     );
